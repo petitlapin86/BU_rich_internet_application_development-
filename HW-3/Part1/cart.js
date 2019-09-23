acb4f9c6e64e57715b071cbb8e4b2401a7cb498b
petitlapin86 <skinnerpa86@yahoo.com>
2019-09-23 11:46:33 -0400

    comment code?

--- HW-3/Part1/cart.js
+++ HW-3/Part1/cart.js
@@ -1,33 +1,34 @@
-var myModule = angular.module('shoppingCart', []);
+var myModule = angular.module('shoppingCart', []); //define module
 
 myModule.controller('CartController', function CartController($scope) {
     if (localStorage.He_cart) {
-        $scope.books = JSON.parse(localStorage.getItem("He_cart"));
+        $scope.books = JSON.parse(localStorage.getItem("He_cart")); //grab from local storage
     } else{
-        $scope.books = [
+        $scope.books = [ //otherwise grab these defined books
             {
-                title: "Absolute Java",
+                title: "Absolute Java", //book 1
                 qty: 1,
                 price: 114.95
             },
             {
-                title: "Pro HTML5",
+                title: "Pro HTML5", //book 2
                 qty: 2,
                 price: 27.95
             },
             {
-                title: "Head First HTML5",
+                title: "Head First HTML5", //book 3
                 qty: 1,
                 price: 27.89
             }
         ];
     }
 
-    $scope.getTotalPrice = function() {
-        var totalPrice = 0;
+    // GET TOTAL PRICE
+    $scope.getTotalPrice = function() { //define a function
+        var totalPrice = 0; //begin price at zero
 
-        for(var i=0; i<$scope.books.length; i++) {
-            totalPrice += $scope.books[i].price * $scope.books[i].qty;
+        for(var i=0; i<$scope.books.length; i++) { //iterate over all books with for loop
+            totalPrice += $scope.books[i].price * $scope.books[i].qty; //find the price of each book and consider the quantity
         }
 
         totalPrice = totalPrice.toFixed(2);
@@ -35,23 +36,26 @@ myModule.controller('CartController', function CartController($scope) {
         return totalPrice
     }
 
-    $scope.books.totalPrice = $scope.getTotalPrice();
+    $scope.books.totalPrice = $scope.getTotalPrice(); //
 
+    //ADD A NEW BOOK
     $scope.addBook = function() {
-        $scope.books.push({
-            title: "New Book",
-            qty: 1,
-            price: 10.99
+        $scope.books.push({ //push onto books array
+            title: "New Book", //default title
+            qty: 1, //default quantity
+            price: 10.99 //default price
         });
-        $scope.getTotalPrice();
+        $scope.getTotalPrice();  //update price total
     }
 
-    $scope.removeBook = function(index) {
-        $scope.books.splice(index, 1);
-        $scope.getTotalPrice();
+    // REMOVE A BOOK
+    $scope.removeBook = function(index) { //define function
+        $scope.books.splice(index, 1); //cut index out of books array
+        $scope.getTotalPrice(); //update price total
     }
 
-    $scope.saveBook = function() {
-        localStorage.setItem("He_cart", JSON.stringify($scope.books));
+    //SAVE A BOOK
+    $scope.saveBook = function() { //define function
+        localStorage.setItem("He_cart", JSON.stringify($scope.books)); //save item to local storage
     }
 });
